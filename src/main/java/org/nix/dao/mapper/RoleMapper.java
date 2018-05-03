b3bcc113386197606318e06180c69bd4a64e9c82
zhangpei <zhangpe0312@qq.com>
2018-05-03 18:14:13 +0800

    用户新增地址字段，并且各个测试方法放入test文件夹中，如果有需求，自己新增测试方法

--- src/main/java/org/nix/dao/mapper/RoleMapper.java
+++ src/main/java/org/nix/dao/mapper/RoleMapper.java
@@ -4,10 +4,16 @@ import org.nix.dao.base.BaseMapper;
 import org.nix.model.RoleModel;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author Kiss
  * @date 2018/05/01 15:28
  */
 @Repository
 public interface RoleMapper extends BaseMapper<RoleModel> {
+
+    void insertRoleList(List<RoleModel> roleModels);
+
+    RoleModel selectRoleByName(String roleName);
 }
