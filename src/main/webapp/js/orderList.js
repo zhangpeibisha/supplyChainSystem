e022f5661eb6029db4ba4d4412e16af0c7bfb3f6
tangdaijia <253141924@qq.com>
2018-05-05 16:08:58 +0800

    分页

--- src/main/webapp/js/orderList.js
+++ src/main/webapp/js/orderList.js
@@ -45,8 +45,8 @@ $(function () {
 
     //查询
     $("#btn_query").click(function (){
-        oTable.Init();
-       /* $.ajax({
+
+        $.ajax({
             type: 'get',
             url: "/api/order/get.do",
             dataType: 'json',
@@ -62,7 +62,7 @@ $(function () {
                 $('#orderTable').bootstrapTable('append', data.list);
 
             }
-        })*/
+        })
     });
 
     //修改
@@ -118,7 +118,7 @@ $(function () {
                     url: '/api/order/update.do',
                     data: {
 
-                        "id":storage["id"],
+                        "id":temp[0].id,
                         "goodsName": goodsName.val(),
                         "unitPrice": unitPrice.val(),
                         "needAmount": needAmount.val(),
@@ -172,14 +172,14 @@ var TableInit = function () {
             sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
             pageNumber:1,                       //初始化加载第一页，默认第一页
             pageSize: 10,                       //每页的记录行数（*）
-            pageList: [10, 25, 50, 100],         //可供选择的每页的行数（*）
+            pageList: [5, 10],         //可供选择的每页的行数（*）
             search: false,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
             strictSearch: true,
             showColumns: true,                  //是否显示所有的列
             showRefresh: true,                  //是否显示刷新按钮
             minimumCountColumns: 2,             //最少允许的列数
             clickToSelect: true,                //是否启用点击选中行
-            height: 500,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+            height: 526,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
             uniqueId: "ID",                     //每一行的唯一标识，一般为主键列
             showToggle:true,                    //是否显示详细视图和列表视图的切换按钮
             cardView: false,                    //是否显示详细视图
@@ -244,9 +244,10 @@ var TableInit = function () {
     oTableInit.queryParams = function (params) {
         var temp = {   //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
             limit: params.limit,   //页面大小
-            curPage: params.offset+1,  //页码
+            curPage: parseInt(params.offset)/parseInt(params.limit)+1,  //页码
             nickName: $("#nickName").val(),
             goodsName:$("#findGoodsName").val()
+
         };
         return temp;
     };
--- src/main/webapp/js/supplierList.js
+++ src/main/webapp/js/supplierList.js
@@ -9,7 +9,6 @@ $(function () {
     oButtonInit.Init();
 
 
-
     //删除
     $("#btn_delete").click(function (){
         var temp= $("#supplierTable").bootstrapTable('getSelections');
@@ -76,10 +75,6 @@ $(function () {
                 //   $(".selectpickerEdit" ).selectpicker('refresh');
                }
            });
-
-
-
-           console.log(temp[0])
 
            //勾选id对应的那条数据内容注入到弹框里面
            $("#nickNameEdit").val(temp[0].nickName);
@@ -233,14 +228,14 @@ var TableInit = function () {
             sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
             pageNumber:1,                       //初始化加载第一页，默认第一页
             pageSize: 10,                       //每页的记录行数（*）
-            pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+            pageList: [5, 10],        //可供选择的每页的行数（*）
             search: false,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
             strictSearch: true,
             showColumns: true,                  //是否显示所有的列
             showRefresh: true,                  //是否显示刷新按钮
             minimumCountColumns: 2,             //最少允许的列数
             clickToSelect: true,                //是否启用点击选中行
-            height: 500,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+            height: 526,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
             uniqueId: "ID",                     //每一行的唯一标识，一般为主键列
             showToggle:true,                    //是否显示详细视图和列表视图的切换按钮
             cardView: false,                    //是否显示详细视图
@@ -299,10 +294,12 @@ var TableInit = function () {
             url: "/api/materialMerchants/get.do",
             dataType: 'json',
             data: {
+                limit: 10,   //页面大小
+                curPage: 1,  //页码
                 goodsName: $("#Name").val()
             },
             success: function(data){
-
+                console.log(data);
                 $('#supplierTable').bootstrapTable('removeAll');
 
                 $('#supplierTable').bootstrapTable('append',data.list);
@@ -313,13 +310,10 @@ var TableInit = function () {
     //得到查询的参数
     oTableInit.queryParams = function (params) {
         var temp = {   //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
-            /*limit: params.limit,   //页面大小
-            offset: params.offset,*/  //页码
-            goodsName: $("#Name").val(),  //名称
-           /* departmentname: $("#txt_search_departmentname").val(),
-            statu: $("#txt_search_statu").val()*/
+            limit: params.limit,   //页面大小
+            curPage: parseInt(params.offset)/parseInt(params.limit)+1,  //页码
+            goodsName: $("#Name").val()  //名称
         };
-        console.log("here:"+temp.goodsName);
         return temp;
     };
     return oTableInit;
